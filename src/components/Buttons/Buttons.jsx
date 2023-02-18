ce78a206e1b95153b7a03d39e5bae1b548fa3132
Alexander Shabanov <dffleh1995@gmail.com>
2023-02-18 20:53:26 +0200

    fix

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,6 +1,4 @@
-import PropTypes from 'prop-types';
 import StyledButton from './Button.styled';
-import { BTN_NAME } from '../../utils/constants';
 
 function Button({ name, handleAction, children }) {
   return (
@@ -9,18 +7,5 @@ function Button({ name, handleAction, children }) {
     </StyledButton>
   );
 }
-
-Button.propTypes = {
-  children: PropTypes.node,
-  name: PropTypes.oneOf([
-    BTN_NAME.REGISTER,
-    BTN_NAME.LOGIN,
-    BTN_NAME.ORANGE,
-    BTN_NAME.WHITE,
-    BTN_NAME.CONFIRM,
-  ]),
-  disabled: PropTypes.bool,
-  handleAction: PropTypes.func,
-};
 
 export default Button;
