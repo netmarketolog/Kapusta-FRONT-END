e4844a4443114953335f12e3bdbf7d4a05b9c0c7
Dmytro Soroka <dimasoroka17@gmail.com>
2023-02-21 23:24:48 +0200

    список категорий

---
+++ src/components/ProductCategory/ProductCategori.jsx
@@ -0,0 +1,29 @@
+// import { useEffect } from 'react';
+import { useState } from 'react';
+import { SelectCustom } from './ProductCategori';
+import { defaultItems } from './DefaultItem';
+import React from 'react';
+export const ProductCategori = () => {
+  const [categories, setCategories] = useState('label category');
+  const getValue = () => {
+    return categories
+      ? defaultItems.find(categorie => categorie.value === categories)
+      : '';
+  };
+  const onChangr = newValue => {
+    setCategories(newValue.value);
+  };
+  console.log(categories);
+  return (
+    <div>
+      <SelectCustom
+        classNamePrefix="custom-select"
+        onChange={onChangr}
+        value={getValue()}
+        options={defaultItems}
+        isSearchable={false}
+        placeholder={'label category'}
+      />
+    </div>
+  );
+};
