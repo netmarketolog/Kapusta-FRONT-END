ef803a76ddb7ba0d06ddebcbeca3d72a0bb6557b
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2023-02-23 13:24:56 +0200

    added dispatch for login/register

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-axios.defaults.baseURL = '';
+axios.defaults.baseURL = 'https://kapusta-wwgw.onrender.com';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
