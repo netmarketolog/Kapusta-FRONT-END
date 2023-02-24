47ec1023e4a7fe2a40855630e025e18285eff544
Maksym Dorohokuplya <dorogmaks@gmail.com>
2023-02-24 16:24:41 +0200

    add reports button
    1) додав кнопку reports button

---
+++ src/components/ChangeBalance/ReportsButton/ReportsBtn.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useLocation } from 'react-router';
+import { ReportsLink, ReportsText } from './ReportsBtn.styled';
+
+import { ReactComponent as ReportsSvg } from '../../../images/icons/ReportsSvg.svg';
+
+const ReportsBtn = () => {
+  const location = useLocation();
+
+  return (
+    <ReportsLink to="/reports" state={{ from: location }}>
+      <ReportsText>Reports</ReportsText>
+      <ReportsSvg />
+    </ReportsLink>
+  );
+};
+
+export default ReportsBtn;
---
+++ src/components/ChangeBalance/ReportsButton/ReportsBtn.styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const ReportsLink = styled(NavLink)`
+  display: flex;
+  align-items: baseline;
+  justify-content: center;
+
+  margin-bottom: 43px;
+
+  font-size: 12px;
+  line-height: 14px;
+  text-decoration: none;
+  color: rgba(82, 85, 95, 0.7);
+
+  transition-property: color;
+  transition-duration: 250ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+
+  :hover,
+  :focus {
+    color: #ff751d;
+  }
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 0;
+  }
+`;
+
+export const ReportsText = styled.span`
+  margin-right: 20px;
+`;
