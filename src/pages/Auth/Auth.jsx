5998a39354da2bd8a870ca04214729ad0360b1e4
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2023-02-25 13:42:57 +0200

    Updated authPage

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,38 +1,10 @@
-// import { ButtonMain } from 'components/Buttons/ButtonMain';
-// import Button from 'components/Buttons/Buttons';
 import { Container } from 'components/Container/Container';
 import { AuthForm } from 'components/Forms/AuthForm/AuthForm';
-// import styled from 'styled-components';
-
-// const TestStyled = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
 
 export const AuthPage = () => {
-  //   const testing = e => {
-  //     e.preventDefault();
-  //   };
   return (
-    // <TestStyled>
     <Container>
-      {/* <Button name="login" handleAction={testing}>
-          Login
-        </Button>
-        <Button name="register" handleAction={testing}>
-          Register
-        </Button>
-        <Button name="white" handleAction={testing}>
-          Input
-        </Button>
-        <Button name="confirm" handleAction={testing}>
-          Confirmation
-        </Button> */}
-
       <AuthForm></AuthForm>
-      {/* <ButtonMain></ButtonMain> */}
     </Container>
-    // </TestStyled>
   );
 };
