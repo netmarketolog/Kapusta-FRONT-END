f39ac76cb4418d87f69ba8c81036ab87a3ca3bef
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2023-02-26 21:16:43 +0200

    Update DatePicker.jsx

--- src/components/Expenses/DatePicker/DatePicker.jsx
+++ src/components/Expenses/DatePicker/DatePicker.jsx
@@ -1,10 +1,9 @@
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css'
-import { useState, useEffect } from 'react';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useState } from 'react';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { DateWrapper } from '../DatePicker/DatePicker.styled';
-import style from '../DatePicker/DatePicker.module.css'
-
+import style from '../DatePicker/DatePicker.module.css';
 
 export const Calendar = () => {
   const [startDate, setStartDate] = useState(new Date());
@@ -15,14 +14,10 @@ export const Calendar = () => {
       <DatePicker
         maxDate={new Date()}
         selected={startDate}
-        className={ style.pickerStyle}
+        className={style.pickerStyle}
         onChange={date => setStartDate(date)}
         dateFormat="dd/MM/yyyy"
       />
     </DateWrapper>
   );
 };
-
-
-
-  
