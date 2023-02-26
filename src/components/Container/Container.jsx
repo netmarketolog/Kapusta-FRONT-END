1ad5400863826c9b2926831e758abb4c0e4d30a1
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2023-02-26 16:36:51 +0200

    Update Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,4 +1,4 @@
-import BgAuth from '../Bacground/BgAuth/BgAuth';
+// import BgAuth from '../Bacground/BgAuth/BgAuth';
 import HeroTitle from 'components/HeroTitle/HeroTitle';
 import { Cont } from './Container.styled';
 import BgUnAuth from 'components/Bacground/BgUnAuth/BgUnAuth';
