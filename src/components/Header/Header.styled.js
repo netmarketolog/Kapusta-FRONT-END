ffe999a7637f904b2d22a588476abd8d793c4b29
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2023-02-27 23:35:20 +0200

    Update Header.styled.js

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -5,8 +5,16 @@ export const HeaderStyled = styled.header`
   justify-content: space-between;
   align-items: center;
   padding: 12px 20px;
+  width: 320px;
+  margin: auto;
 
   @media screen and (min-width: 768px) {
     padding: 12px 32px;
+    width: 768px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding: 12px 16px;
+    width: 1280px;
   }
 `;
