1fa16c894e366ebb6c41714d2634aa7943064e47
Nadiia Kotyk <99249549+Nandy7777@users.noreply.github.com>
2023-02-28 20:08:48 +0200

    add user email

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,6 +3,7 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectBalance = state => state.auth.user.balance;
 export const selectIsRefreshing = state => state.auth.isRefreshing;
 export const selectTokenDeadline = state => state.auth.token?.expiresIn;
+export const selectEmail = state => state.auth.user.email;
 
 // Transactions
 export const selectTransactions = state => state.transactions.items;
