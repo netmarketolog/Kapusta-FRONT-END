35363f16b1b3347245838fb1d2d7a4ae8e5aeaa4
Vetali1985 <100028292+Vetali1985@users.noreply.github.com>
2023-02-28 22:57:16 +0200

    fix

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 
 export const Cont = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  align-items: center;
   position: absolute;
   z-index: -1;
   width: 100%;
@@ -14,12 +8,11 @@ export const Cont = styled.div`
   border-radius: 0 0 0 45px;
   background-color: #f2f5fc;
   @media screen and (min-width: 768px) {
-    align-items: stretch;
     padding-left: 50px;
     padding-right: 50px;
     height: 526px;
   }
   @media screen and (min-width: 1280px) {
-    height: 600px;
+    height: 582px;
   }
 `;
--- src/components/Container/ContainerAuth.jsx
+++ src/components/Container/ContainerAuth.jsx
@@ -1,6 +1,5 @@
 import BgAuth from 'components/Bacground/BgAuth/BgAuth';
 import { Cont } from './Container.styled';
-// import BgUnAuth from 'components/Bacground/BgUnAuth/BgUnAuth';
 
 export const ContainerAuth = ({ children }) => (
   <Cont>
