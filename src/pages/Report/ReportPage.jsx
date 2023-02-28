5e02cbc1d6924f17171e850f173939ac9a6eb11c
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2023-02-28 20:20:46 +0200

    connected diagram

--- src/pages/Report/ReportPage.jsx
+++ src/pages/Report/ReportPage.jsx
@@ -1,18 +1,24 @@
-import { arr } from 'components/Reports/Diagram/arr';
+// import { arr } from 'components/Reports/Diagram/arr';
 import { Diagram } from 'components/Reports/Diagram/Diagram';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RefreshUser } from 'redux/auth/authOperations';
-import { selectTokenDeadline } from 'redux/selectors';
+import { selectReports, selectTokenDeadline } from 'redux/selectors';
 import { getReport } from 'redux/transactions/transactionsOperations';
 import { ExpensesIncomes } from '../../components/ExpensesIncomes/ExpensesIncomes';
 import { ChangeBalance } from '../../components/ChangeBalance/ChangeBalance';
+
 const ReportPage = () => {
+  const { statistics } = useSelector(selectReports);
+
+  const [category, setCategory] = useState('');
+  const [stats, setStats] = useState([]);
+  const [operation, setOperation] = useState('expense');
+  const [year, setYear] = useState(2023);
+  const [month, setMonth] = useState(2);
+
   const dispatch = useDispatch();
   const deadline = useSelector(selectTokenDeadline);
-  const operation = 'expense';
-  const year = 2023;
-  const month = 2;
 
   useEffect(() => {
     (async function fetchData() {
@@ -21,15 +27,22 @@ const ReportPage = () => {
       }
       await dispatch(getReport({ operation, year, month }));
     })();
-
     console.log('UseEffect in Report.js!!!!!');
-  }, [deadline, dispatch]);
+  }, [deadline, dispatch, month, operation, year]);
+
+  useEffect(() => {
+    if (statistics && statistics[0]) {
+      setCategory(statistics[0]._id);
+      const [data] = statistics.filter(item => item._id === category);
+      setStats(data?.stats);
+    }
+  }, [category, statistics]);
 
   return (
     <>
       <ChangeBalance />
-      <ExpensesIncomes stats={arr} />
-      <Diagram stats={arr} />
+      <ExpensesIncomes />
+      {stats && stats.length > 0 && <Diagram stats={stats} />}
     </>
   );
 };
