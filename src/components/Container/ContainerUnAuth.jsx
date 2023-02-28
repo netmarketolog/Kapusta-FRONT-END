21be23e00494f51b74e848b182b1b1745bcac530
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2023-03-01 00:31:53 +0200

    Update ContainerUnAuth.jsx

--- src/components/Container/ContainerUnAuth.jsx
+++ src/components/Container/ContainerUnAuth.jsx
@@ -1,12 +1,12 @@
 import BgUnAuth from 'components/Bacground/BgUnAuth/BgUnAuth';
 import { Cont } from './Container.styled';
-import { Wrrapper } from './Wrapper.styled';
+// import { Wrrapper } from './Wrapper.styled';
 
 export const ContainerUnAuth = ({ children }) => (
-  <Wrrapper>
-    <Cont>
-      {children}
-      <BgUnAuth></BgUnAuth>
-    </Cont>
-  </Wrrapper>
+  // <Wrrapper>
+  <Cont>
+    {children}
+    <BgUnAuth></BgUnAuth>
+  </Cont>
+  // </Wrrapper>
 );
