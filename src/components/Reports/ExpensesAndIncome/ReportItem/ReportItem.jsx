cef766faf2ecaeae4aa67083531569c56b1332ad
Vadym Liubushkin <dffleh1995@gmail.com>
2023-03-02 00:54:20 +0200

    fix .toLowerCase()

--- src/components/Reports/ExpensesAndIncome/ReportItem/ReportItem.jsx
+++ src/components/Reports/ExpensesAndIncome/ReportItem/ReportItem.jsx
@@ -8,13 +8,14 @@ import {
 import Icons from '../../../../images/icons/spriteReportsSvg.svg';
 
 export const ReportItem = ({ name, total, setCategory }) => {
+  console.log(name);
   return (
     <ReportItemButton>
       <Sum>{total}</Sum>
       <Wrapper>
         <ButtonReport onClick={() => setCategory(name)} name={name}>
           <svg width="60" height="60">
-            <use href={`${Icons}#icon-${name}`}></use>
+            <use href={`${Icons}#icon-${name.toLowerCase()}`}></use>
           </svg>
           <div></div>
         </ButtonReport>
