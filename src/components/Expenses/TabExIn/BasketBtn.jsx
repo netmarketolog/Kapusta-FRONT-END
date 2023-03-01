a3406e5a42ed0bf1c06673eff2f00318dba78eb0
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2023-03-01 17:32:35 +0200

    fixed logic

--- src/components/Expenses/TabExIn/BasketBtn.jsx
+++ src/components/Expenses/TabExIn/BasketBtn.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { ReactComponent as DeleteIcon } from '../../../images/icons/DeleteIconSvg.svg';
 import { ModalAsk } from 'components/Modal/ModalAsk/ModalAsk';
 import { deleteTransaction } from 'redux/transactions/transactionsOperations';
+import { RefreshUser } from 'redux/auth/authOperations';
+import { selectTokenDeadline } from 'redux/selectors';
 
 export const BasketBtn = ({ transactionId }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [currentId, setCurrentId] = useState(null);
+  const deadline = useSelector(selectTokenDeadline);
 
   const dispatch = useDispatch();
 
@@ -20,9 +23,13 @@ export const BasketBtn = ({ transactionId }) => {
     setModalOpen(false);
   };
 
-  const handleDelete = evt => {
+  const handleDelete = async () => {
     const transactionId = currentId;
-    dispatch(deleteTransaction({ transactionId }));
+    if (deadline) {
+      if (Date.now() >= deadline) await dispatch(RefreshUser());
+    }
+    await dispatch(deleteTransaction({ transactionId }));
+    await dispatch(RefreshUser());
     setCurrentId(null);
     setModalOpen(false);
   };
