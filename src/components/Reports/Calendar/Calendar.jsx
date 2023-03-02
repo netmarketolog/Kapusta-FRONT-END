8e6be95b8790d3cb0146cecbc04b683bd7b72581
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2023-03-02 12:09:47 +0200

    Update Calendar.jsx

--- src/components/Reports/Calendar/Calendar.jsx
+++ src/components/Reports/Calendar/Calendar.jsx
@@ -3,7 +3,11 @@ import Icons from '../../../images/icons/spriteReportsSvg.svg';
 // import { PropTypes } from 'prop-types';
 //      <CalendarReport month={month} year={year} changeDate={changeDate} />
 
-export const CalendarReport = ({ month, year, changeDate }) => {
+export const CalendarReport = ({
+  month = 'MARCH',
+  year = '2023',
+  changeDate,
+}) => {
   return (
     <Wrap>
       <Text>Current Texteriod:</Text>
